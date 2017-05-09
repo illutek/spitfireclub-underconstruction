5edad26337f982e35d25c710657a594f4f365298
Stefaan Vandenborne <stefanvandenborne@gmail.com>
2017-05-09 13:02:14 +0200

    layout 2 kolommen

--- index.php
+++ index.php
@@ -1,61 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="css/styles.min.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Spitfireclubvlaanderenvzw.</title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="css/styles.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Spitfireclubvlaanderenvzw.</title>
 </head>
 <body>
-<section class="container">
-    <div class="content">
-        <div class="row">
+<section class="container-fluid">
+  <div class="row wrapper">
+    <div class="col-md-6 col-sm-8 left">
+      <div class="left-wrap">
+        <div class="content">
+          <div class="row">
             <div class="header">
-                <div class="col-md-2 logo">
-                    <img src="images/logo.png" alt="LOGO Spitfireclubvlaanderen">
-                </div>
-                <div class="col-md-10">
-                    <h1>Spitfireclubvlaanderenvzw</h1>
-                </div>
+              <div class="col-md-12 logo">
+                <img src="images/logo.png" alt="LOGO Spitfireclubvlaanderen">
+              </div>
+              <div class="col-md-12">
+                <h1>Spitfireclubvlaanderenvzw</h1>
+              </div>
             </div>
 
             <div class="col-md-12">
-                <div class="offline-message">
-                    <p>Wegens software upgrade van onze hosting is onze site offline, er wordt aan een nieuwe site gebouwd
-                        om terug helemaal mee te zijn.</p>
-                </div>
-                <div class="messages-wrapper">
+              <div class="offline-message">
+                <p>Wegens software upgrade van onze hosting is onze site offline, er wordt aan een nieuwe site gebouwd
+                  om terug helemaal mee te zijn.</p>
+              </div>
+              <div class="messages-wrapper">
 
-                </div>
-                <div class="contact-details">
-                    <h4>Onze contact gegevens</h4>
-                    <a href="mailto:info@spitfireclubvlaanderenvzw.be">
-                        <i class="fa fa-envelope-o"></i> info@spitfireclubvlaanderenvzw.be
-                    </a><br>
-                    <a href="mailto:inschrijvingen@spitfireclubvlaanderenvzw.be">
-                        <i class="fa fa-envelope-o"></i> inschrijvingen@spitfireclubvlaanderenvzw.be
-                    </a><br>
-                    <a href="mailto:voorzitter@spitfireclubvlaanderenvzw.be">
-                        <i class="fa fa-envelope-o"></i> voorzitter@spitfireclubvlaanderenvzw.be
-                    </a><br>
-                    <a href="tel:+32495548565"><i class="fa fa-phone"></i> 0495548565</a>
-                </div>
+              </div>
+              <div class="contact-details">
+                <h4>Onze contact gegevens</h4>
+                <a href="mailto:info@spitfireclubvlaanderenvzw.be">
+                  <i class="fa fa-envelope-o"></i> info@spitfireclubvlaanderenvzw.be
+                </a><br>
+                <a href="mailto:inschrijvingen@spitfireclubvlaanderenvzw.be">
+                  <i class="fa fa-envelope-o"></i> inschrijvingen@spitfireclubvlaanderenvzw.be
+                </a><br>
+                <a href="mailto:voorzitter@spitfireclubvlaanderenvzw.be">
+                  <i class="fa fa-envelope-o"></i> voorzitter@spitfireclubvlaanderenvzw.be
+                </a><br>
+                <a href="tel:+32495548565"><i class="fa fa-phone"></i> 0495548565</a>
+              </div>
 
             </div>
+          </div>
         </div>
+        <footer class="footer-wrap">
+          <div class="row">
+            <div class="col-md-8 col-sm-8 col-xs-8 copyright">
+                <?php date_default_timezone_set('Europe/Brussels'); ?>
+              <i class="fa fa-copyright"></i> Spitfireclubvlaanderenvzw | <?php echo date("Y"); ?>
+            </div>
+            <div class="col-md-4 col-sm-4 col-xs-4 facebook">
+              <a href="https://www.facebook.com/groups/spitfireclubvlaanderen/">
+                <img src="images/facebook.png" alt="Facebook Spitfireclubvlaanderen">
+              </a>
+            </div>
+          </div>
+        </footer>
+      </div>
+
     </div>
-    <footer class="footer-wrap">
-        <div class="row">
-            <div class="col-md-8 copyright">
-                <i class="fa fa-copyright"></i> Spitfireclubvlaanderenvzw | <?php echo date("Y"); ?>
-            </div>
-            <div class="col-md-4 facebook">
-                <a href="https://www.facebook.com/groups/spitfireclubvlaanderen/">
-                    <img src="images/facebook.png" alt="Facebook Spitfireclubvlaanderen">
-                </a>
-            </div>
-        </div>
-    </footer>
+    <div class="col-md-6 col-sm-4 image-wrap">
+
+    </div>
+  </div>
+
+
 </section>
 </body>
 </html>
