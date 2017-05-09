79c6d77d3a5e2db95b8b2232fec06f964f052ff6
Vandenborne Stefan <stefan@illutek.be>
2017-05-09 10:30:38 +0200

    stand online 20170509

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/styles.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Spitfireclubvlaanderenvzw.</title>
+</head>
+<body>
+<section class="container">
+    <div class="content">
+        <div class="row">
+            <div class="header">
+                <div class="col-md-2 logo">
+                    <img src="images/logo.png" alt="LOGO Spitfireclubvlaanderen">
+                </div>
+                <div class="col-md-10">
+                    <h1>Spitfireclubvlaanderenvzw</h1>
+                </div>
+            </div>
+
+            <div class="col-md-12">
+                <div class="offline-message">
+                    <p>Wegens software upgrade van onze hosting is onze site offline, er wordt aan een nieuwe site gebouwd
+                        om terug helemaal mee te zijn.</p>
+                </div>
+                <div class="messages-wrapper">
+
+                </div>
+                <div class="contact-details">
+                    <h4>Onze contact gegevens</h4>
+                    <a href="mailto:info@spitfireclubvlaanderenvzw.be">
+                        <i class="fa fa-envelope-o"></i> info@spitfireclubvlaanderenvzw.be
+                    </a><br>
+                    <a href="mailto:inschrijvingen@spitfireclubvlaanderenvzw.be">
+                        <i class="fa fa-envelope-o"></i> inschrijvingen@spitfireclubvlaanderenvzw.be
+                    </a><br>
+                    <a href="mailto:voorzitter@spitfireclubvlaanderenvzw.be">
+                        <i class="fa fa-envelope-o"></i> voorzitter@spitfireclubvlaanderenvzw.be
+                    </a><br>
+                    <a href="tel:+32495548565"><i class="fa fa-phone"></i> 0495548565</a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <footer class="footer-wrap">
+        <div class="row">
+            <div class="col-md-8 copyright">
+                <i class="fa fa-copyright"></i> Spitfireclubvlaanderenvzw | <?php echo date("Y"); ?>
+            </div>
+            <div class="col-md-4 facebook">
+                <a href="https://www.facebook.com/groups/spitfireclubvlaanderen/">
+                    <img src="images/facebook.png" alt="Facebook Spitfireclubvlaanderen">
+                </a>
+            </div>
+        </div>
+    </footer>
+</section>
+</body>
+</html>
